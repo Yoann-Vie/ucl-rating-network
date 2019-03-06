84dc8d8030e2ca28bc039111dd64ab7d3044594b
Pierre-Yves DERET <pierreyves.deret@gmail.com>
2019-03-06 14:34:10 +0100

    Call back + front

--- client/src/components/LoginFormContainer.js
+++ client/src/components/LoginFormContainer.js
@@ -10,6 +10,16 @@ class LoginFormContainer extends React.Component {
 
     handleSubmit = () => {
         console.log(this.state);
+        fetch('http://127.0.0.1:3000/login_check', {
+            method: "POST",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(this.state)
+        }).then((response) => response.json())
+        .then(data => localStorage.setItem("token", data.token))
+        .catch(error => console.log(error));
     }
 
     handleChange = (value,field) => {
