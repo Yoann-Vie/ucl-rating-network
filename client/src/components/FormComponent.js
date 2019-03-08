5a19f18a79e1c3a0e00fc0039f6747776c1cf6c0
Yoann <contact@yoannvie.fr>
2019-03-08 15:50:56 +0100

    add ability to rate a match

--- client/src/components/FormComponent.js
+++ client/src/components/FormComponent.js
@@ -8,13 +8,15 @@ class FormComponent extends React.Component {
         super(props);
 
         this.state = {
-            comment: ''
+            comment: '',
+            rate: null,
         };
 
-        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleSubmitComment = this.handleSubmitComment.bind(this)
+        this.handleSubmitRate = this.handleSubmitRate.bind(this)
     }
 
-    handleSubmit = (event) => {
+    handleSubmitComment = (event) => {
         event.preventDefault()
         fetchData("POST", "match/comment", localStorage.getItem('token'), {
             "team1_key": this.props.match.team1.key,
@@ -30,37 +32,57 @@ class FormComponent extends React.Component {
         })
     }
 
-    handleChange = (value) => {
+    handleSubmitRate = (event) => {
+        event.preventDefault()
+        if (this.state.rate === null) {
+            return false
+        }
+        fetchData("POST", "match/rate", localStorage.getItem('token'), {
+            "team1_key": this.props.match.team1.key,
+            "team2_key": this.props.match.team2.key,
+            "year": this.props.year,
+            "round": this.props.round,
+            "rating": this.state.rate
+        }).then((updatedMatch) => {
+            this.props.updateModalMatch(updatedMatch)
+            this.setState({
+                rate: null
+            })
+        })
+    }
+
+    handleChangeComment = (value) => {
         this.setState({
             comment: value
         });
     }
 
+    handleChangeRate = (value) => {
+        this.setState({
+            rate: value
+        });
+    }
+
     render() {
+        let options = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((optionValue, key) => {
+            return <option key={key}>{optionValue}</option>
+        })
+
         return (
             <div>
-                <Form>
+                <Form onSubmit={this.handleSubmitRate}>
                     <FormGroup>
                         <Label for="form_rating">Notation :</Label>
-                        <Input type="select" name="select" id="form_rating">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                            <option>6</option>
-                            <option>7</option>
-                            <option>8</option>
-                            <option>9</option>
-                            <option>10</option>
+                        <Input type="select" name="select" id="form_rating" onChange={(event) => this.handleChangeRate(event.currentTarget.value)}>
+                            { options }
                         </Input>
                     </FormGroup>
                     <Button className="submit">Rate</Button>
                 </Form>
-                <Form onSubmit={this.handleSubmit}>
+                <Form onSubmit={this.handleSubmitComment}>
                     <FormGroup>
                         <Label for="form_comment">Commentaires</Label>
-                        <Input value={this.state.comment} type="textarea" name="text" id="form_comment" onChange={(event) => this.handleChange(event.currentTarget.value)} />
+                        <Input value={this.state.comment} type="textarea" name="text" id="form_comment" onChange={(event) => this.handleChangeComment(event.currentTarget.value)} />
                     </FormGroup>
                     <Button className="submit">Post</Button>
                 </Form>
