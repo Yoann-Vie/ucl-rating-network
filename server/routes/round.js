8e088209d6db7235a90035323d2caf02682f06b5
NegreriePaul <paul.negrerie@gmail.com>
2019-03-08 16:25:57 +0100

    fix try

--- server/routes/round.js
+++ server/routes/round.js
@@ -4,11 +4,26 @@ const Round = require('../models/round')
 
 router.get('/rounds', (req, res) => {
 
-    Round.find(req.query).sort({ 'matches.date': -1 })
-        .then((items) => {
-            res.status(200).send(items)
+    Round.find({ year: req.query.year }).sort({ 'matches.date': -1 })
+        .then((rounds) => {
+
+            rounds.map((round) => {
+
+                round.matches.map((match) => {
+
+                    let tabAvg = 0;
+                    match.ratings.map((rating) => {
+
+                        tabAvg += rating.rate
+                    })
+                    match.avgRate = tabAvg/match.ratings.length
+                })
+            })
+            res.status(200).send(rounds)
         })
         .catch((error) => console.log('Error while retrieving rounds details: ' + error))
 })
 
 module.exports = router
+
+
