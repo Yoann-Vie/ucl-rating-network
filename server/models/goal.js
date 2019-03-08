6d46b52f36efc553326294dc635698ed74ec014d
NegreriePaul <paul.negrerie@gmail.com>
2019-03-08 10:29:37 +0100

    goal

--- server/models/goal.js
+++ server/models/goal.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Rating = require('./rating')
 
 const Schema = mongoose.Schema
 
@@ -6,7 +7,8 @@ const goalSchema = new Schema({
     name: String,
     minute: {type: Number, min: 0, max: 150},
     score1: {type: Number, min: 0},
-    score2: {type: Number, min: 0}
+    score2: {type: Number, min: 0},
+    ratings: [Rating]
 })
 
 module.exports = goalSchema
