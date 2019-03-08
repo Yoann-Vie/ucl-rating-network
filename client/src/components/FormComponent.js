023b8f61bedfc867b4d3fe148ad415f542eb3df5
Yoann <contact@yoannvie.fr>
2019-03-08 14:50:44 +0100

    flush comment after submit

--- client/src/components/FormComponent.js
+++ client/src/components/FormComponent.js
@@ -35,28 +35,33 @@ class FormComponent extends React.Component {
 
     render() {
         return (
-          <Form onSubmit={this.handleSubmit}>
-            <FormGroup>
-              <Label for="form_rating">Notation :</Label>
-              <Input type="select" name="select" id="form_rating">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-                <option>6</option>
-                <option>7</option>
-                <option>8</option>
-                <option>9</option>
-                <option>10</option>
-              </Input>
-            </FormGroup>
-            <FormGroup>
-              <Label for="form_comment">Commentaires</Label>
-              <Input type="textarea" name="text" id="form_comment" onChange={(event) => this.handleChange(event.currentTarget.value)} />
-            </FormGroup>
-            <Button className="submit">Submit</Button>
-          </Form>
+            <div>
+                <Form>
+                    <FormGroup>
+                        <Label for="form_rating">Notation :</Label>
+                        <Input type="select" name="select" id="form_rating">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                            <option>6</option>
+                            <option>7</option>
+                            <option>8</option>
+                            <option>9</option>
+                            <option>10</option>
+                        </Input>
+                    </FormGroup>
+                    <Button className="submit">Rate</Button>
+                </Form>
+                <Form onSubmit={this.handleSubmit}>
+                    <FormGroup>
+                        <Label for="form_comment">Commentaires</Label>
+                        <Input value={this.state.comment} type="textarea" name="text" id="form_comment" onChange={(event) => this.handleChange(event.currentTarget.value)} />
+                    </FormGroup>
+                    <Button className="submit">Post</Button>
+                </Form>
+            </div>
         );
     }
 }
